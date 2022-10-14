da311ec200ccbfa446f80d5bce616954ffbd2366
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-10-14 12:19:34 +0300

    Update cu.cpp

--- cu.cpp
+++ cu.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	vector<int> cu {23,45,67,82,89};
-	for(vector<int>::iterator i = cu.begin();i!=v.end();i++){
+	for(vector<int>::iterator i = cu.begin();i!=cu.end();i++){
 		cout<<*i<<" ";
 	}
 	
